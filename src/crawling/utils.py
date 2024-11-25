1b47b15a96eec17f8ace4ad5b4aebb77ee097bd6
sunwoo <summerham22@gmail.com>
2024-11-25 14:14:04 +0900

    test

--- src/crawling/utils.py
+++ src/crawling/utils.py
@@ -16,7 +16,7 @@ def get_last_id_from_redis(default_id=51820):
     return int(last_id)
     #return None
 
-def update_last_id_in_redis(new_id):
+def update_last_id_in_redis(new_id=51820):
     r = connect_to_redis()
     r.set('last_processed_id', new_id)
 
