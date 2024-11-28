40e6a6ad50ff96f7a370edd625890399d8e9f95a
sunwoo <summerham22@gmail.com>
2024-11-28 15:04:02 +0900

    test

--- src/crawling/open_page.py
+++ src/crawling/open_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from detail_page import *
+from crawling.detail_page import *
 
 
 options = webdriver.ChromeOptions()
